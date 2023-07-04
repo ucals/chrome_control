bc7696ff7f63818ff158a45bd8bfa8d066a5879e
Carlos Souza <souza@gatech.edu>
2023-07-04 14:45:05 -0300

    completing the package and adding tests

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name='chrome_control',
-    version='0.1.0',
-    package_dir = {"": "chrome_control"},
+    version='0.1.1',
+    package_dir={"": "chrome_control"},
     install_requires=[
         'requests',
         'pyautogui'
