01b486f46b5068b4c243d08c2eb2c9022c321ed2
Carlos Souza <souza@gatech.edu>
2023-07-07 12:03:45 -0300

    disabling pyautogui failsafe

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 
 setup(
     name='chrome_control',
-    version='0.1.7',
+    version='0.1.8',
     entry_points={
         'console_scripts': [
             'chrome-control = chrome_control:main',
+            'ccc = chrome_control:main',
         ]
     },
     install_requires=[
